65973a089205dfa9d94e1857db3e4cc765b31158
briSalvador <salvadorbriannab@gmail.com>
2025-02-18 15:48:43 +0800

    Tools + program file

---
+++ program.py
@@ -0,0 +1,8 @@
+path = "C:\Windows\System32\winevt\Logs"
+evtxPath = "./EvtxECmd/EvtxeCmd/EvtxECmd.exe"
+
+option = ""
+
+while(option != "exit"):
+    option = input("Input option")
+    print(option)
